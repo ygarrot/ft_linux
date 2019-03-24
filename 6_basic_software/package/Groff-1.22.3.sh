6763183538f5743141563cb57e7b798598ab88e4
yoan garrot <garrot.yoan1@gmail.com>
2019-03-24 16:45:17 +0100

    fix little typo

--- 6_basic_software/package/Groff-1.22.3.sh
+++ 6_basic_software/package/Groff-1.22.3.sh
@@ -3,7 +3,7 @@
 
 # Prepare Groff for compilation:
 
-PAGE=<paper_size> ./configure --prefix=/usr
+PAGE=A4 ./configure --prefix=/usr
 # This package does not support parallel build. Compile the package:
 
 make -j1
