4bcd5e2e4553650e7aa4453086acfdd488d9d6c8
yoan garrot <garrot.yoan1@gmail.com>
2019-04-15 17:33:16 +0200

    linux upd

--- 6_basic_software/package/Linux-4.18.5_API_Headers.sh
+++ 6_basic_software/package/Linux-4.18.5_API_Headers.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-# The Linux kernel needs to expose an Application Programming Interface (API) for the system's C library (Glibc in LFS) to use. This is done by way of sanitizing various C header files that are shipped in the Linux kernel source tarball.
+install -v -m755 -d /etc/modprobe.d
+cat > /etc/modprobe.d/usb.conf << "EOF"
+# Begin /etc/modprobe.d/usb.conf
 
-# Make sure there are no stale files and dependencies lying around from previous activity:
+install ohci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i ohci_hcd ; true
+install uhci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i uhci_hcd ; true
 
-make mrproper
-# Now extract the user-visible kernel headers from the source. They are placed in an intermediate local directory and copied to the needed location because the extraction process removes any existing files in the target directory. There are also some hidden files used by the kernel developers and not needed by LFS that are removed from the intermediate directory.
-
-make INSTALL_HDR_PATH=dest headers_install
-find dest/include \( -name .install -o -name ..install.cmd \) -delete
-cp -rv dest/include/* /usr/include
+# End /etc/modprobe.d/usb.conf
+EOF
